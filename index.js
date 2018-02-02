40a787abf6a158a6e30309dba9a26ba13d54d2c1
Soumya Anantha Padmanabha <soumya_anantha@yahoo.com>
2018-02-02 23:35:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function whisper(string){
   return string.toLowerCase();
 }
 function logShout(string){
-  console.log(string.toLowerCase());
+  console.log(string.toUpperCase());
 }
 function logWhisper(string){
   console.log(string.toLowerCase());
