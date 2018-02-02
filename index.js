07dae2191edcb09218dc1751045abbb9ac7449e4
Soumya Anantha Padmanabha <soumya_anantha@yahoo.com>
2018-02-02 23:08:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function shout(string) {
-  return string.toUpperCase()
+function sayHiToGrandma() {
+  var lowercase = 'whisper';
+  lowercase.toLowerCase() === lowercase
+  //var logShout;
+  //var logWhisper;
+  //var sayHiToGrandma;
+  //return string.toUpperCase()
 }
 /*describe('shout(string)', () => {
   it('receives one argument and returns it in all caps', () => {
