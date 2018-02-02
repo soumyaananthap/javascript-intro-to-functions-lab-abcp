20f0be071ffb9cb324d44864df7b27cb8d8165e9
Soumya Anantha Padmanabha <soumya_anantha@yahoo.com>
2018-02-02 23:23:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function sayHiToGrandma(string) {
-  if(string.toLowerCase() === lowercase){
+  if(string.toLowerCase() === string){
     return "I can't hear you!"
-  } else if(string.toUpperCase() === uppercase){
+  } else if(string.toUpperCase() === string){
     return "YES INDEED!"
   } else if(string === "I love you, Grandma."){
     return "I love you, too."
